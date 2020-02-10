f2cb3b1667c5e5d735228585e104d68bd3d6ac88
Mike LEUNG <ckleungah@gmail.com>
2020-02-10 20:07:22 +0800

    add America

--- driving.py
+++ driving.py
@@ -1,8 +1,13 @@
 country = input('where are you from: ')
 age = input('what is your age: ')
-age = int(age) #casting 
+age = int(age) #casting change from string to integer
 if country == 'Hongkong':
 	if age >= 18:
+		print('you can take driving examination')
+	else: 
+		print('you are not qualified for the driving examination')
+elif country == 'America':
+	if age >= 16:
 		print('you can take driving examination')
 	else: 
 		print('you are not qualified for the driving examination')
